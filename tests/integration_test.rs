34a33110a2ed8f5368c27b0e968eefe1a3374b26
Allan McInnes <allanmcinnes@users.noreply.github.com>
2019-11-29 14:10:40 +1300

    Move to using generic iterators instead of 'signals', since this makes the functionality more general purpose. Establish trait bounds necessary for the iterated values to undergo convolution.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,11 +4,10 @@ use linear_systems::primitives::{fir, signal};
 #[test]
 fn test_filter_chain() {
     let mut data = vec![1.0, 2.0, 3.0, 4.0, 5.0].into_iter();
-    let mut xs = signal::Signal::new(&mut data);
-    let mut filt1 = fir::Fir::new(vec![0.5, 0.5], &mut xs);
-    let mut x2s = filt1.output();
-    let mut filt2 = fir::Fir::new(vec![1.], &mut x2s);
-    let ys = signal::Signal::new(&mut filt2);
+    let mut filt1 = fir::Fir::new(vec![0.5, 0.5], &mut data);
+    let x2s = filt1.output();
+    let mut filt2 = fir::Fir::new(vec![1.], x2s);
+    let ys = filt2.output();
 
     let expected = vec![0.5, 1.5, 2.5, 3.5, 4.5, 2.5];
     for (y, ex) in ys.zip(expected) {
